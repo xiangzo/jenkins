76c1fb4025b5f9da41bc1ec60430ca6a46324b56
Bunga <72001593+xiangzo@users.noreply.github.com>
2022-05-27 12:49:39 +0700

    add laravel

---
+++ resources/views/user/layouts/design.blade.php
@@ -0,0 +1,34 @@
+<div class="section">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-3 mb-4">
+          <div class="service" data-aos="fade-up" data-aos-delay="">
+            <span class="icon icon-magnet mb-4 d-block"></span>
+            <h3>Designing</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias id volume amet.</p>
+          </div>
+        </div>
+        <div class="col-lg-3 mb-4">
+          <div class="service" data-aos="fade-up" data-aos-delay="100">
+            <span class="icon icon-screen-desktop mb-4 d-block"></span>
+            <h3>UI / UX</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias id volume amet.</p>
+          </div>
+        </div>
+        <div class="col-lg-3 mb-4">
+          <div class="service" data-aos="fade-up" data-aos-delay="200">
+            <span class="icon icon-screen-smartphone mb-4 d-block"></span>
+            <h3>Web</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias id volume amet.</p>
+          </div>
+        </div>
+        <div class="col-lg-3 mb-4">
+          <div class="service" data-aos="fade-up" data-aos-delay="300">
+            <span class="icon icon-magnifier mb-4 d-block"></span>
+            <h3>IOT</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias id volume amet.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
